9ed063b2d3995357418c6056f97b6d8560c814cd
lucasgiovine <lucas.giovine23@gmail.com>
2024-05-04 23:03:16 -0300

    create task model and tasks atribute in user model

--- src/main/java/com/lucasfalcone/FirstSpringBootProject/models/User.java
+++ src/main/java/com/lucasfalcone/FirstSpringBootProject/models/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,6 +14,8 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -44,7 +47,8 @@ public class User {
     @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -79,6 +83,14 @@ public class User {
         this.password = password;
     }
 
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -104,5 +116,4 @@ public class User {
         return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
                 && Objects.equals(this.password, other.password);
     }
-
 }
