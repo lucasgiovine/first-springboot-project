5efa5cbc335bb5cece1562ebdc669b457c5df01c
lucasgiovine <lucas.giovine23@gmail.com>
2024-05-11 20:25:11 -0300

    format file

--- src/main/java/com/lucasfalcone/FirstSpringBootProject/repositories/TaskRepository.java
+++ src/main/java/com/lucasfalcone/FirstSpringBootProject/repositories/TaskRepository.java
@@ -12,13 +12,10 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByUser_Id(Long id); // Query using JpaRepository interface
 
-    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id") // Query using
-    // JPQL (from Spring)
+    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id") // Query using JPQL (from Spring)
     // List<Task> findByUser_Id(@Param("id") Long id);
 
-    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = //
-    // Query using SQL
-    // true)
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)      // Query using SQL
     // List<Task> findByUser_Id(@Param("id") Long id);
 
 }
